17831df5b2c228b083e90cc933ecf3f470dcc30c
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-02-27 10:36:55 -0500

    added files

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Jibryll Brinkley on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
